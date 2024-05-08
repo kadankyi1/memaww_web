837829603aee73215d460fdfb318bc27d8e0bd62
kadankyi1 <annodankyikwaku@gmail.com>
2024-05-08 05:28:00 -0700

    Changed home page title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>MeMaww - A Laundry Company</title>
+	<title>MeMaww - A Laundry Service</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
