5ef13486f7e3c3f6814f3801757e81453c605109
kadankyi1 <annodankyikwaku@gmail.com>
2024-05-08 04:48:55 -0700

    Fixed links on all pages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,7 +26,7 @@
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 		<div class="container">
-			<a class="navbar-brand" href="index.html" style="font-family: MeMaww; font-size: 30px">MeMaww<span style="font-family: sans-serif">A Laundry Company</span></a>
+			<a class="navbar-brand" href="index.html" style="font-family: MeMaww; font-size: 30px">MeMaww<span style="font-family: sans-serif">A Laundry Service</span></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="oi oi-menu"></span> Menu
 			</button>
